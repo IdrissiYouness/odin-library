780276f1f76167211d648916ae9b5ec4eb2ecc06
Youness Sghiouri Idrissi <zahirkamarul@outlook.com>
2024-09-12 01:12:56 +0100

    improve js calling functions

--- library.js
+++ library.js
@@ -12,33 +12,45 @@ class Book {
 
 
 const  myLibrary = [];
+let newBook;
 
 
-Book.prototype.addBookToLibrary = function(book = new Book(title,author,pages,isRead,isArchived)){
-  myLibrary.push(book)
+Book.prototype.addBookToLibrary = function(){
+  myLibrary.push(this)
 }
 
-Book.prototype.archiveBook = function(book){
+Book.prototype.archiveBook = function(){
     this.isArchived = !this.isArchived;
 }
 
-
-let ebook = new Book();
-
-ebook.addBookToLibrary(ebook);
-ebook.archiveBook(ebook);
-
-myLibrary.forEach((book,index) => {
-  console.log (`Book ${index+1} :`);
-  console.log(`isArchived ${book.isArchived}`);
-});
-
-function showPopUp (id) {
+function TogglePopUp (id) {
   const popup = document.getElementById(id);
   popup.classList.toggle("show");
 }
 
-// event Listeners
+ const addNewBookBtn = document.querySelector('.btn.addBookBtn');
 
- const addBookBtn = document.querySelector('.add-book-btn');
+
+
+ addNewBookBtn.addEventListener('click', (event)=>{
+
+    event.preventDefault();
+
+    const title = document.querySelector("#title-input").value;
+    const author = document.querySelector("#author-input").value;
+    const numberOfPages = document.querySelector("#pages-input").value;
+    const isRead = document.querySelector("#read-input").checked;
+
+    newBook = new Book(title,author,numberOfPages,isRead);
+    newBook.addBookToLibrary();
+    console.log(myLibrary);
+
+ });
+
+
+
+
+
+
+
 
